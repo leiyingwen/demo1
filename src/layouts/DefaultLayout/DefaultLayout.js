4bc8605dd92a26d4652568918f809061c0e4144f
leiyingwen <53306701@qq.com>
2019-10-30 16:40:05 +0800

    第一次提提交

---
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './DefaultLayout.less'
+
+export default class DefaultLayout extends Component {
+    render() {
+        return (
+            <div id="DefaultLayout">
+                HelloWorld!
+            </div>
+        )
+    }
+}
